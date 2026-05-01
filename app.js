8ec9550bdd147aa9ffbddb7be92b609e31feb061
Suraj Kumar <surajroy04032005@gmail.com>
2026-05-01 17:29:44 +0530

    port problem

--- app.js
+++ app.js
@@ -101,6 +101,8 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message)
 })
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
     console.log("app is listening on port 8080")
 })
